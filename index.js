f8f579821496555f5c6d91ccab40247949ef7f2c
Baby Nap Boltron <147931096+babynapboltron8@users.noreply.github.com>
2025-03-01 13:09:59 +0800

    modified html form

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = () => {
+    return `${Book.title}, ${Book.author}, ${Book.pages} not read yet.`;
+  };
 }
+
+const book1 = new Book('The Stars', 'Ryan', 350);
+const book2 = new Book('Bible', 'Jesus', 1000);
 
 function addBookToLibrary() {
   // take params, create a book then store it in the array
