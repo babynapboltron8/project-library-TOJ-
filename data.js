872a88582b276b5022b2e42abbd630aed4f4a8f6
Baby Nap Boltron <147931096+babynapboltron8@users.noreply.github.com>
2025-03-02 16:17:46 +0800

    created data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const books = [
+  {
+    id: 1,
+    title: 'Human Hack',
+    author: 'Alice Johnson',
+    pages: 265,
+    desc: '',
+  },
+  {
+    id: 2,
+    title: 'The Hax',
+    author: 'George Bush',
+    pages: 343,
+    desc: '',
+  },
+  {
+    id: 3,
+    title: 'The Test',
+    author: 'Paul Screw',
+    pages: 190,
+    desc: '',
+  },
+];
